4c6bd43a2207537e577697c5fa1dfbd133b1991b
fillipe59 <fillipelimaps@gmail.com>
2021-10-16 12:36:41 -0300

    regfile.vhd v2.1

--- Atividade 2/regfile.vhd
+++ Atividade 2/regfile.vhd
@@ -23,27 +23,18 @@ architecture arch_regfile of regfile is
 	type mem_tipo is array(0 to regn-1) of bit_vector(wordSize-1 downto 0);
 	signal regs: mem_tipo; --registradores
 	
-	signal int_wr: integer;
-	signal int_rr1: integer;
-	signal int_rr2: integer;
 begin
-	int_wr <= to_integer(unsigned(wr));
-	int_rr1 <= to_integer(unsigned(rr1));
-	int_rr2 <= to_integer(unsigned(rr2));
-	
-	process(clock, reset)
+	process(clock, reset, rr1, rr2)
 	begin
 		if reset = '1' then
 			regs <= (others => bit_vector(to_unsigned(0, wordSize)));
-			q1 <= regs(int_rr1);
-			q2 <= regs(int_rr2);
 		elsif clock = '1' and clock'event then
 			if regWrite = '1' then
-				regs(int_wr) <= d; 
+				regs(to_integer(unsigned(wr))) <= d; 
 			end if;
-			regs(regn-1) <= (others => '0');
-			q1 <= regs(int_rr1);
-			q2 <= regs(int_rr2);
 		end if;
+		regs(regn-1) <= (others => '0');
+		q1 <= regs(to_integer(unsigned(rr1)));
+		q2 <= regs(to_integer(unsigned(rr2)));
 	end process;
 end arch_regfile;
