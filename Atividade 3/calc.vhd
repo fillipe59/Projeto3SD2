9cf7394f7772e7139e75339447102ba0f6271a8b
fillipe59 <fillipelimaps@gmail.com>
2021-10-17 00:56:08 -0300

    calc.vhd e calc_tb.vhd v 1.1

--- Atividade 3/calc.vhd
+++ Atividade 3/calc.vhd
@@ -27,26 +27,26 @@ architecture arch_calc of calc is
 		);
 	end component;
 	
-	signal q1_out, q2_out, resultado: bit_vector(15 downto 0);
+	signal oper1, roper2, resultado: bit_vector(15 downto 0);
 begin
 	bancoDeRegs: regfile 
 					generic map(32, 16) 
 					port map(clock, reset, '1', 
 							instruction(14 downto 10), instruction(9 downto 5), 
-							instruction(4 downto 0), resultado, q1_out, q2_out);
+							instruction(4 downto 0), resultado, roper2, oper1);
 	
-	q1 <= resultado;
-	process(clock, reset)
+	q1 <= oper1;
+	process(clock)
 	begin
 		if clock = '1' and clock'event then
 			if instruction(16 downto 15) = "00" then --! ADD
-				resultado <= bit_vector(unsigned(q2_out) + unsigned(q1_out));
+				resultado <= bit_vector(signed(roper2) + signed(oper1));
 			elsif instruction(16 downto 15) = "01" then --! ADDI
-				resultado <= bit_vector(unsigned(q2_out) + to_integer(signed(instruction(14 downto 10))));
+				resultado <= bit_vector(signed(oper1) + to_integer(signed(instruction(14 downto 10))));
 			elsif instruction(16 downto 15) = "10" then --! SUB
-				resultado <= bit_vector(unsigned(q2_out) - unsigned(q1_out));
+				resultado <= bit_vector(signed(oper1) - signed(roper2));
 			else --! SUBI
-				resultado <= bit_vector(unsigned(q2_out) - to_integer(signed(instruction(14 downto 10))));
+				resultado <= bit_vector(signed(oper1) - to_integer(signed(instruction(14 downto 10))));
 			end if;
 		end if;
 	end process;
@@ -79,7 +79,7 @@ begin
 	process(clock, reset)
 	begin
 		if reset = '1' then
-			regs <= (others => bit_vector(to_unsigned(0, wordSize)));
+			regs <= (others => bit_vector(to_signed(0, wordSize)));
 		elsif clock = '1' and clock'event then
 			if regWrite = '1' then
 				regs(to_integer(unsigned(wr))) <= d; 
