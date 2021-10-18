1dac875f247c7feb0a892e2732e5a19725fa696c
fillipe59 <fillipelimaps@gmail.com>
2021-10-18 17:36:06 -0300

    melhora da legibilidade de calc.vhd

--- Atividade 3/calc.vhd
+++ Atividade 3/calc.vhd
@@ -14,27 +14,34 @@ architecture arch_calc of calc is
 	signal regs: mem_tipo; --registradores	
 	
 	signal opcode: bit_vector(1 downto 0);
-	signal oper1, oper2, dest: bit_vector(4 downto 0);
+	signal oper1, oper2, dest: integer range 0 to 31;
+	
+	signal q_oper1, q_oper2: signed(15 downto 0);
+	signal imediato: integer;
 begin
 	opcode <= instruction(16 downto 15);
-	oper2 <= instruction(14 downto 10);
-	oper1 <= instruction(9 downto 5);
-	dest <= instruction(4 downto 0);
+	oper2 <= to_integer(unsigned(instruction(14 downto 10)));
+	oper1 <= to_integer(unsigned(instruction(9 downto 5)));
+	dest <=  to_integer(unsigned(instruction(4 downto 0)));
+	
+	q_oper1 <= signed(regs(oper1));
+	q_oper2 <= signed(regs(oper2));
+	imediato <= to_integer(signed(instruction(14 downto 10)));
 
-	q1 <= regs(to_integer(unsigned(oper1)));
+	q1 <= regs(oper1);
 	process(clock)
 	begin
 		if reset = '1' then 
 			regs <= (others => bit_vector(to_signed(0, 16)));
 		elsif clock = '1' and clock'event then
 			if opcode = "00" then --! ADD
-				regs(to_integer(unsigned(dest))) <= bit_vector(signed(regs(to_integer(unsigned(oper1)))) + signed(regs(to_integer(unsigned(oper2)))));
+				regs(dest) <= bit_vector(q_oper1 + q_oper2);
 			elsif opcode = "01" then --! ADDI
-				regs(to_integer(unsigned(dest))) <= bit_vector(signed(regs(to_integer(unsigned(oper1)))) + to_integer(signed(oper2)));
+				regs(dest) <= bit_vector(q_oper1 + imediato);
 			elsif opcode = "10" then --! SUB
-				regs(to_integer(unsigned(dest))) <= bit_vector(signed(regs(to_integer(unsigned(oper1)))) - signed(regs(to_integer(unsigned(oper2)))));
+				regs(dest) <= bit_vector(q_oper1 - q_oper2);
 			else --! SUBI
-				regs(to_integer(unsigned(dest))) <= bit_vector(signed(regs(to_integer(unsigned(oper1)))) - to_integer(signed(oper2)));
+				regs(dest) <= bit_vector(q_oper1 - imediato);
 			end if;
 		end if;
 		regs(31) <= (others => '0');
